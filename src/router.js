d9acb839b0909070f3020c0fbb024989ddcbfe51
tip <lokasun@icloud.com>
2018-11-26 16:19:58 +0700

    start

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Products from './views/Products.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -18,6 +18,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: Products
     }
   ]
 })
